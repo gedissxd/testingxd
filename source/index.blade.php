c7306106d06b64d071a58ebba6e40a3be99e9a33
Gediminas <gediss.lolas78@gmail.com>
2024-12-25 06:01:28 +0200

    sada

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,6 +3,6 @@
 @section('body')
 <div class="p-10 flex">
     <h1 class="text-3xl font-bold text-yellow-500 ">Hello world!</h1>
-    <h1 class="text-3xl font-bold ">Hello world!</h1>
+    <h1 class="text-xl font-bold ">Hello world!</h1>
 </div>
 @endsection
